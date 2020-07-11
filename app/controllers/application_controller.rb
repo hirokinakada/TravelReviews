e67b45e009521fa2ad9d85b763b1916b0a407366
hirokinakada <dudu.hiccc@gmail.com>
2020-07-11 09:18:53 +0000

    add,admin/home,about

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < ActionController::Base
 	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name ,:first_name ,:last_name_kana ,:first_name_kana ,:nickname ,:email ])
   #sign_upの際にnameのデータ操作を許。追加したカラム。
 	end
+
+	def after_sign_in_path_for(resource)
+		case resource
+		when Admin
+		  admins_home_path
+		when Customer
+		  root_path
+		end
+	  end
+  
+	  def after_sign_out_path_for(resource)
+		if resource == :admin
+		new_admin_session_path
+	  else
+		  new_customer_session_path
+	  end
+  end
 end
