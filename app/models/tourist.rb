44d74cb4f1a47734aad776eef580314596ae2a15
hirokinakada <dudu.hiccc@gmail.com>
2020-07-18 02:50:42 +0000

    add.search

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,4 +1,18 @@
 class Tourist < ApplicationRecord
     attachment :image, destroy: false
     has_many :spots, dependent: :destroy
+
+    def Tourist.search(search, tourist_or_spot, how_search)
+        if how_search == "1"
+            Tourist.where(['name LIKE ?', "%#{search}%"])
+        elsif how_search == "2"
+            Tourist.where(['name LIKE ?', "%#{search}"])
+        elsif how_search == "3"
+            Tourist.where(['name LIKE ?', "#{search}%"])
+        elsif how_search == "4"
+            Tourist.where(['title LIKE ?', "#{search}"])
+        else
+            Tourist.all
+        end
+    end
 end
